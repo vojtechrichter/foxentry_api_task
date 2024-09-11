88fcefd1f15750ff6eb3fd65b73cf1a424506917
vojtechrichter <vojta3218@gmail.com>
2024-09-11 13:56:58 +0200

    registered openapi spec controller

--- src/index.php
+++ src/index.php
@@ -2,15 +2,16 @@
 
 namespace FoxentryApiTask;
 
-use Elastic\Elasticsearch\ClientBuilder;
 use Elastic\Elasticsearch\Exception\AuthenticationException;
-use Elastic\Elasticsearch\Exception\ElasticsearchException;
 use FoxentryApiTask\CustomerInterface\CustomerController;
+use FoxentryApiTask\OpenapiSpec\OpenApiSpecController;
 use FoxentryApiTask\ProductRegistry\ProductRegistryController;
 use FoxentryApiTask\ProductStorage\ElasticProductsController;
 use Slim\Factory\AppFactory;
 
 require(__DIR__ . '/../vendor/autoload.php');
+
+error_reporting(0);
 
 try {
     ElasticProductsController::init();
@@ -19,7 +20,6 @@ try {
 }
 
 $app = AppFactory::create();
-$app->addErrorMiddleware(true, true, true);
 
 $app->get('/products', ProductRegistryController::class . ':getProducts');
 $app->get('/product/{id}', ProductRegistryController::class . ':getProduct');
@@ -30,5 +30,6 @@ $app->post('/buy/{id}', CustomerController::class . ':buyProduct');
 $app->get('/generate_id', CustomerController::class . ':generateId');
 $app->get('/purchases', ProductRegistryController::class . ':getPurchases');
 $app->get('/search/{name}', ProductRegistryController::class . ':searchByName');
+$app->get('/api_spec', OpenApiSpecController::class . ':renderOpenApiSpec');
 
 $app->run();
